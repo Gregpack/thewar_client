36cfa78a9e71141b17bff8acc01cc5c006706b33
Grigory Karavaev <gregpack0@gmail.com>
2022-08-11 01:18:14 +0700

    thewar-client

--- your_ai.h
+++ your_ai.h
@@ -10,7 +10,7 @@ using namespace thewar::dto;
 using namespace thewar::actions;
 using namespace thewar::orders;
 
-OrderMessage get_next_move(GameStateDto& game_state, int player_id);
+OrderMessage get_next_move(GameStateDto game_state, int player_id);
 std::string get_name();
 
 #endif //JNI_CLIENT_YOUR_AI_H
